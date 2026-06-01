83b4101c96a7218809fc21fd1002f9bc680ec2bf
mafer1707 <mafercasmen@gmail.com>
2026-06-01 10:08:40 -0400

    Agregar archivos de proyecto.

---
+++ PlanificadorDeProcesos/Class_PlanificadorDeProcesos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanificadorDeProcesos
+{
+    internal class Class_PlanificadorDeProcesos
+    {
+    }
+}
---
+++ PlanificadorDeProcesos/Form1.cs
@@ -0,0 +1,10 @@
+namespace PlanificadorDeProcesos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
