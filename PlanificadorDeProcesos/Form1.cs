54ca7311c5725df7be980e50c15f483acabd36ae
mafer1707 <mafercasmen@gmail.com>
2026-06-01 15:54:18 -0400

    form agregado al control de versiones

---
+++ PlanificadorDeProcesos/Form1.cs
@@ -0,0 +1,15 @@
+namespace PlanificadorDeProcesos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pnl_Generador_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
